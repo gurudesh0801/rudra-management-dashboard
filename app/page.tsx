019d95f0dd311982d661db7c6bd9eb253d40c8b5
Amit Bagaitkar <165465376+abagaitkar@users.noreply.github.com>
2025-09-17 23:43:57 +0530

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
-import Image from "next/image";
-import LoginPage from "./login/page";
+// app/page.tsx
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return <LoginPage />;
+  redirect("/login");
 }
